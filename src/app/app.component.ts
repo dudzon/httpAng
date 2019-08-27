8908e7ccf9948d57c8baf98253f202a8eae6c05d
rj <rj@gw-griddynamics.retn.net>
2019-08-27 16:51:38 +0200

    http module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,11 @@ export class AppComponent implements OnInit {
   ngOnInit() {}
 
   onCreatePost(postData: { title: string; content: string }) {
+    console.log(postData);
     // Send Http request
     this.http
       .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        'https://ng-complete-guide-b1645.firebaseio.com/posts.json',
         postData
       )
       .subscribe(responseData => {
